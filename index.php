c83258c348015f87d0a80565ef7f8f2ee73a13f6
Stendy Sakur <sakur.stendy@gmail.com>
2022-07-12 20:37:44 +0800

    kembali ke nama footer group kelas b

--- index.php
+++ index.php
@@ -89,7 +89,7 @@ getErrorLogin();
 		<p><h5>Sistem Informasi Perpustakaan (sipus) | Praktikum </h5></p>
 		<p><h6><small>
 		Kasus asli dari Kominfo & Panitia VSGA <br />
-		Modifikasi oleh __test_update <br />
+		Modifikasi oleh YELLOW TIM 2022 <br />
 		Logmodif: menggunakan Bootsrap sederhana dan Mengubah Teknik Pemrograman dengan menggunakan konsep Subrutin / modular</small></h6></p>
 	</div>
 </body>
